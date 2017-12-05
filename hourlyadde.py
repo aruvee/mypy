a5400118848acf09bf43d41915cf9e9241cff4d3
aruvee <aruvee@gmail.com>
2017-12-05 12:42:51 +0530

    hourlyadde.py

---
+++ hourlyadde.py
@@ -0,0 +1,14 @@
+import mechanicalsoup
+from Myemail import Myemail
+import json
+browser = mechanicalsoup.StatefulBrowser()
+response=browser.open("https://www.nseindia.com/live_market/dynaContent/live_watch/stock_watch/niftyStockWatch.json")
+output = json.loads(response.text)
+advance=output["advances"]
+decline=output["declines"]
+subject = "Advance: " + str(advance) + " Decline: "+ str(decline)
+myemail=Myemail()
+myemail.send_email("aruna","aruna","veera",subject,output)
+
+
+
