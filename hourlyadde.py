5f3dded9a515e05ed0bd529df0e1ac7690a145cf
aruvee <aruvee@gmail.com>
2017-12-12 06:33:36 +0530

    modified the message

--- hourlyadde.py
+++ hourlyadde.py
@@ -9,7 +9,12 @@ decline = output["declines"]
 
 subject = "Advance: " + str(advance) + " Decline: " + str(decline)
 
-message = output["latestData"]
+counter = 0
+message = " "
+while counter < 50:
+    message = message + output["data"][counter]["symbol"] + " " + output["data"][counter]["per"] + "\n"
+    counter = counter + 1
+print(message)
 
 myemail = Myemail()
 myemail.send_email("aruna", "aruna", "veera", subject, message)
