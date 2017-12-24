240e4e8b0d519f8bb97ac4addca1d79b1662e6ed
aruvee <aruvee@gmail.com>
2017-12-24 18:53:07 +0530

    changed the trend subject

--- Nifty50.py
+++ Nifty50.py
@@ -9,3 +9,22 @@ class Nifty50:
         response = browser.open(url)
         output = json.loads(response.text)
         return output
+
+    def populate(self):
+        browser = mechanicalsoup.StatefulBrowser()
+        url = "https://www.nseindia.com/live_market/dynaContent/live_watch/stock_watch/niftyStockWatch.json"
+        response = browser.open(url)
+        output = json.loads(response.text)
+        #print(output)
+        #file = open("nifty50.txt", "w")
+        with open('nifty50.json', 'w') as outfile:
+            json.dump(output, outfile)
+        #file.write(str(output))
+        #print("After write")
+        return True
+
+    def getBankIndex(self):
+        bankvalue = ""
+        data = json.load(open('nifty50.json'))
+        bankvalue = data["advances"]
+        return  bankvalue
