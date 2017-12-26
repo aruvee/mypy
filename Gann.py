aedba5576f839647c508e2c0e00d9e771930b866
aruvee <aruvee@gmail.com>
2017-12-26 08:33:47 +0530

    Initial version

--- Gann.py
+++ Gann.py
@@ -1,7 +1,11 @@
+from nsetools import Nse
 import math
+import os
 #current = input("What is the current price")
-current = input("Get the current nse value")
-current = int(current)
+#current = input("Get the current nse value")
+nse = Nse()
+nifty = nse.get_index_quote('NIFTY 50')
+current = int(nifty['lastPrice'])
 sqrtvalue = int(math.sqrt(current))
 starting = sqrtvalue - 1
 #print(starting)
@@ -31,15 +35,20 @@ while listcounter < listcount:
 #print(index)
 buylist = []
 selllist = []
-newcounter = 0
-while newcounter < 5:
-    buylist.append(gannlist[index+newcounter])
-    newcounter = newcounter + 1
-    selllist.append(gannlist[index-newcounter])
-print("Buy Targets")
-for value in buylist:
-    print(int(value))
+newcounter = 5
+while newcounter >= 0:
+    buylist.append(gannlist[index - 1 + newcounter])
+    newcounter = newcounter - 1
+    selllist.append(gannlist[index - 1 - newcounter])
 
-print("\n Sell Targets")
-for value in selllist:
-    print(int(value))
+if os.stat("buy.txt").st_size == 0:
+    file = open("buy.txt", "w")
+    for value in buylist:
+        file.write(str(value)+"\n")
+    file.close()
+
+if os.stat("sell.txt").st_size == 0:
+    file = open("sell.txt", "w")
+    for value in selllist:
+        file.write(str(value)+"\n")
+    file.close()
