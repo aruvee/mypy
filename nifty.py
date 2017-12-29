d31ce6ff11463322ec6c2a368ed651cf8386c425
aruvee <aruvee@gmail.com>
2017-12-29 09:56:52 +0530

    world index added

--- nifty.py
+++ nifty.py
@@ -2,12 +2,16 @@ from Myemail import Myemail
 from nsetools import Nse
 from datetime import datetime
 import pytz
+from index import  Index
 timestamp=datetime.now(pytz.timezone('Asia/Kolkata'))
 print(timestamp.time(),"Invoked Nifty")
 nse = Nse()
+index = Index()
 myemail=Myemail()
 nifty = nse.get_index_quote('NIFTY 50') 
 banknifty = nse.get_index_quote('NIFTY BANK')
 niftyIT = nse.get_index_quote('NIFTY IT')
 message="Nifty: "+ str(nifty['lastPrice']) + "\n banknifty: " + str(banknifty['lastPrice']) + "\n niftyIT: " + str(niftyIT['lastPrice'])
+message = message + "\n" + index.worldindex()
+#print(message)
 myemail.send_email("aruna","aruna","veera",nifty['lastPrice'],message)
