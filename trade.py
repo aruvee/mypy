0e6f287868ad284a5acd1c81d5f7fb43a370ab49
aruvee <aruvee@gmail.com>
2018-01-10 08:31:37 +0530

    Final trade commit

--- trade.py
+++ trade.py
@@ -1,8 +1,8 @@
 from tradedao import TradeDAO
 tradeDAO = TradeDAO()
 #tradeDAO.insertTrade("index", "NIFTY", 10620)
-type = input("Enter the Type of Symbol")
-symbol = input("Enter the Symbol")
-price = input("Enter the price")
+type = raw_input("Enter the Type of Symbol")
+symbol = raw_input("Enter the Symbol")
+price = raw_input("Enter the price")
 tradeDAO.insertTrade(type, symbol, price)
 #tradeDAO.updateTrade("NIFTY", 10620)
