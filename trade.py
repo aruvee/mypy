1b33c8303252760a742a818341c0f7ef34f463b1
aruvee <aruvee@gmail.com>
2018-01-11 20:09:06 +0530

    alert timing changed

--- trade.py
+++ trade.py
@@ -1,13 +1,13 @@
 from tradedao import TradeDAO
 tradeDAO = TradeDAO()
-entry = input("Enter C/R/D")
+entry = raw_input("Enter C/R/D -----> ")
 
 if entry == "D":
     tradeDAO.delTrade()
 elif entry == "C":
-    stype = input("Enter the Type of Symbol")
-    symbol = input("Enter the Symbol")
-    price = input("Enter the price")
+    stype = raw_input("Enter the Type of Symbol")
+    symbol = raw_input("Enter the Symbol")
+    price = raw_input("Enter the price")
     tradeDAO.insertTrade(stype, symbol, price)
 elif entry == "R":
     cursor = tradeDAO.selectTrade()
