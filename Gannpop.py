393b4ac9583325501d4b3e625d146707fd6f4820
aruvee <aruvee@gmail.com>
2018-01-31 19:09:32 +0530

    Gann Email added

--- Gannpop.py
+++ Gannpop.py
@@ -1,8 +1,10 @@
 import sqlite3
-from MyGann import  MyGann
+from MyGann import MyGann
 from GannDAO import GannDAO
+from Myemail import Myemail
 
 gann = MyGann()
+myemail = Myemail()
 gannDao = GannDAO()
 conn = sqlite3.connect("stock.db")
 cursor = gannDao.selectGann(conn)
@@ -11,3 +13,14 @@ for row in cursor:
     symbol = row[1]
     mylist = gann.getGannValues(stype, symbol)
     gannDao.populateGann(conn, mylist, "N", 0, symbol)
+cursor = gannDao.selectGann(conn)
+subject = "Gann Values"
+message = ""
+for row in cursor:
+    column = len(row)
+    counter = 0
+    while counter < column:
+        message = message + str(row[counter]) + "\n"
+        counter = counter + 1
+    message = message + "\n" + "\n"
+myemail.send_email("Aruna", "Aruna", "Veera", subject, message)
