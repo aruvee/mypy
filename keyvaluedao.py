a5aa96e09522770568c9f8380a871063cc970084
aruvee <aruvee@gmail.com>
2018-02-13 19:43:45 +0530

    Advance trend

---
+++ keyvaluedao.py
@@ -0,0 +1,13 @@
+class keyvaluedao:
+
+    def getValue(self, conn, key):
+        cursor = conn.execute("Select value from keyvalue  where key=?", (key,))
+        value = ""
+        for row in cursor:
+            value = row[0]
+        return value
+
+    def updateValue(self, conn, key, value):
+        conn.execute("UPDATE keyvalue SET value=? where key=?", (value, key))
+        conn.commit()
+
