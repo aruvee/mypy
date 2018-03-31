c3d5954e52da6bbe76113f8f5b51f58a438bc19b
aruvee <aruvee@gmail.com>
2018-03-31 21:10:27 +0530

    Portfolio notify

--- watchalert.py
+++ watchalert.py
@@ -47,8 +47,12 @@ for row in cursor:
 
     diff = datetime.now() -notifyTime
 
-    if change > 0.5 and change < 100 :
-        subject = "Watch Alert " + symbol[:10] + " " + str(currentValue) + " " + str(ltp)
+    if change > 0.5 and change < 100:
+        if ltp > notifyPrice:
+            subject = "Watch Alert +ve "
+        else:
+            subject = "Watch Alert -ve "
+        subject = subject + symbol[:10] + " " + str(currentValue) + " " + str(ltp)
         message = "Symbol " + symbol + "\n" + "BuyPrice " + str(buyPrice) + "\n" + "LTP " + str(ltp) + "\n"
         message = message + "NotifyPrice " + str(notifyPrice) + "\n" + "NotifyValue " + str(notifyValue) + "\n" + "CurrentValue " + str(currentValue)
         watchdao.updateWatch(conn, symbol, ltp)
