03a63f98f873cfbb5466c2a344360ab2849a13be
aruvee <aruvee@gmail.com>
2018-05-10 11:54:00 +0530

    clean file

--- tmfcalc.py
+++ tmfcalc.py
@@ -1,12 +1,11 @@
-import requests
 import json
 import requests
 from python import python
 
+
 @python.route('/api/calculate')
 def calculate():
     r = requests.get("http://18.232.21.157:3000/api/queries/selectAllEvent", stream=True)
-    print(r.content)
     data = json.loads(r.content)
     total = 0
     for mylist in data:
