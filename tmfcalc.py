0e2025a664c57b9f1b5074c1925b529ebe16ec20
aruvee <aruvee@gmail.com>
2018-05-10 11:23:20 +0530

    Flag based stoploss

---
+++ tmfcalc.py
@@ -0,0 +1,14 @@
+import requests
+import json
+import requests
+from python import python
+
+@python.route('/api/calculate')
+def calculate():
+    r = requests.get("http://18.232.21.157:3000/api/queries/selectAllEvent", stream=True)
+    print(r.content)
+    data = json.loads(r.content)
+    total = 0
+    for mylist in data:
+        total = total + float(mylist["ratedProductUsage"]["chargeAmount"])
+    return total
