f25a14506a42db99763e42abd5962bf4bfbe7c32
aruvee <aruvee@gmail.com>
2018-06-07 18:49:59 +0530

    TMF date based

--- tmfcalc.py
+++ tmfcalc.py
@@ -1,11 +1,21 @@
 import json
 import requests
+from flask import request
 from python import python
+
 
 
 @python.route('/api/calculate')
 def calculate():
-    r = requests.get("http://18.232.21.157:3000/api/queries/selectAllEvent", stream=True)
+    isMvnoEvent = request.args.get('isMvnoEvent')
+    isInternationalRoaming = request.args.get('isInternationalRoaming')
+    isNationalRoaming = request.args.get('isNationalRoaming')
+    startDate = request.args.get('startDate') + "T00:01:01"
+    endDate = request.args.get('endDate') + "T23:59:59"
+    urlprefix = "http://18.232.21.157:3000/api/queries/selectTotalBillAmountByDateRange?"
+    url = urlprefix + "startDate=" + startDate + "&endDate=" + endDate + "&isMvnoEvent=" + isMvnoEvent + "&isInternationalRoaming=" + isInternationalRoaming + "&isNationalRoaming=" + isNationalRoaming
+    print(url)
+    r = requests.get(url, stream=True)
     data = json.loads(r.content)
     total = 0
     for mylist in data:
