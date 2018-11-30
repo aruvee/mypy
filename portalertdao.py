a5cd59d75318156f688cdcb96f69cd02cd1cc2cc
veera <kveeramani@gmail.com>
2018-11-30 07:36:15 +0530

    alertflag changes

--- portalertdao.py
+++ portalertdao.py
@@ -6,7 +6,7 @@ class PortAlertDAO:
     now = datetime.now()
 
     def addPortAlert(self, cursor, tdate, symbol, buyprice, percent):
-        query = "INSERT INTO portalert VALUES (%s,%s,%s,%s,now()) ON DUPLICATE KEY " \
+        query = "INSERT INTO portalert VALUES (%s,%s,%s,%s,now(),0) ON DUPLICATE KEY " \
                 "UPDATE tdate = %s, buyprice = %s, percent = %s, moddate = now()"
         data = (tdate, symbol, buyprice, percent, tdate, buyprice, percent)
         cursor.execute(query, data)
