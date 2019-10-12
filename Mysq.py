8d2627e9c7c27c43db10882aeba203d54e3ed866
veera <kveeramani@gmail.com>
2019-10-12 17:46:50 +0530

    for RSI 30 and 70 alerts

--- Mysq.py
+++ Mysq.py
@@ -4,7 +4,7 @@ import mysql.connector
 class Mysq:
 
     def getConnection(self):
-        cnx = mysql.connector.connect(user='veerastock', password='veerastock',
-                                      host='veerastock.clilr2ftkr50.ap-south-1.rds.amazonaws.com',
+        cnx = mysql.connector.connect(user='veera', password='veera',
+                                      host='13.233.94.163',
                                       database='stock')
         return cnx
