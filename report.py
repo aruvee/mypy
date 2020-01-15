360108b5520631ddf351dba103dd3478c873b583
veera <kveeramani@gmail.com>
2020-01-15 14:56:01 +0530

    improvements

--- report.py
+++ report.py
@@ -6,6 +6,7 @@ import sqlite3
 from Mysq import Mysq
 from keyvaluedao_mysql import KeyvalueDAO
 import sys
+from watchdao_mysql import WatchDAO
 
 
 try:
@@ -26,31 +27,44 @@ mysq = Mysq()
 mysqlconn = mysq.getConnection()
 cursor = mysqlconn.cursor()
 flag = keyvaluedao.getValue(cursor, "report")
+watchdao = WatchDAO()
+stocks = watchdao.selectWatch(cursor)
+allstocks = stocks.fetchall()
 flag = int(flag)
+stockName = ""
 
 if flag == interval:
-    print("inside")
-    cursor = reportdao.selectReport(conn)
-    for row in cursor:
-        stype = row[0]
-        symbol = row[1]
-        stime = row[2]
-        etime = row[3]
-        interval = row[4]
-        rtime = row[5]
+    #print("inside")
+    for stock in allstocks:
+        if stock[2] == "stoc":
+            stockName = stock[1]
 
-        isReport = stockutils.isReport(stime, etime, interval, rtime)
-        if isReport:
-            if stype == "gindex":
-               gLlist.append(symbol)
-            else:
-                value = index.getStockPrice(stype, symbol)
-                message = message + symbol + " " + str(value) + "\n"
-            #reportdao.updateReport(conn, symbol)
-
-    if gLlist.__len__() > 0:
-        message = message + "\n" + index.gindex(gLlist)
-
-    if message.__len__() > 0:
-        subject = "Index Report " + str(index.getStockPrice("index", "NIFTY 50"))
+    if stockName != "":
+        print(stockName)
+        subject = stockName +" Price " + str(index.getStockPrice("stock", stockName))
         myemail.send_email("Aruna", "Aruna", "Veera", subject, message)
+    else:
+        cursor = reportdao.selectReport(conn)
+        for row in cursor:
+            stype = row[0]
+            symbol = row[1]
+            stime = row[2]
+            etime = row[3]
+            interval = row[4]
+            rtime = row[5]
+
+            isReport = stockutils.isReport(stime, etime, interval, rtime)
+            if isReport:
+                if stype == "gindex":
+                   gLlist.append(symbol)
+                else:
+                    value = index.getStockPrice(stype, symbol)
+                    message = message + symbol + " " + str(value) + "\n"
+                #reportdao.updateReport(conn, symbol)
+
+        if gLlist.__len__() > 0:
+            message = message + "\n" + index.gindex(gLlist)
+
+        if message.__len__() > 0:
+            subject = "Index Report " + str(index.getStockPrice("index", "NIFTY 50"))
+            myemail.send_email("Aruna", "Aruna", "Veera", subject, message)
